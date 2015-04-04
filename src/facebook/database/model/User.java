d7592d18ea65130615771e45437a16d4e3ece697
congshuW <congshuwang@gmail.com>
2015-04-04 10:52:04 -0400

    added dao and model files

---
+++ src/facebook/database/model/User.java
@@ -0,0 +1,62 @@
+package facebook.database.model;
+
+import java.util.Collection;
+
+import facebook.database.dao.UserDAO;
+
+public class User 
+{
+	private UserDAO dao;
+	private int userId;
+	private String userName;
+	private String password;
+	private String email;
+	
+	public User(UserDAO dao, int userId, String userName, String password, String email)
+	{
+		this.dao = dao;
+		this.userId = userId;
+		this.userName = userName;
+		this.password = password;
+		this.email = email;
+		//...
+	}
+	
+	public int getUserId()
+	{
+		return userId;
+	}
+	
+	public String getUserName()
+	{
+		return userName;
+	}
+	
+	public void changeUserName(String newUserName)
+	{
+		userName = newUserName;
+		//dao.changeUserName(...);
+	}
+	
+	public String getPassword()
+	{
+		return password;
+	}
+	
+	public void changePassword(String newPassword)
+	{
+		password = newPassword;
+		//dao.changePassword(...);
+	}
+	
+	public String getEmail()
+	{
+		return email;
+	}
+	
+	public void changeEmail(String newEmail)
+	{
+		email = newEmail;
+		//dao.changeEmail(...);
+	}
+}
